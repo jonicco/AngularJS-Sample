4faa4df7fb02c8e48f19a8a071b3123eb71cb4c8
Atsushi Watanabe <jonicco@gmail.com>
2014-08-05 07:24:47 +0900

    first commit

---
+++ app/scripts/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name angularSampleApp
+ * @description
+ * # angularSampleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('angularSampleApp', [
+    'ngAnimate',
+    'ngRoute',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
